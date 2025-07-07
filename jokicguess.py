70ec9171634851a2cc883ddf481fd8db54e4d0dc
bogdang989 <bgavra989@gmail.com>
2025-07-07 15:27:53 +0200

    Refresh correct address

--- jokicguess.py
+++ jokicguess.py
@@ -910,9 +910,10 @@ async def swapfest_refresh_points(interaction: discord.Interaction):
     )
         new_points = await swapfest.get_moment_points(FLOW_ACCOUNT, moment_id)
         if new_points > 0:
-        f"✅ Refreshing points for {moment_id}: {new_points}.",
-        ephemeral=True
-    )
+        await interaction.followup.send(
+            f"✅ Refreshing points for {moment_id}: {new_points}.",
+            ephemeral=True
+        )
             # Update the points in DB
             cursor.execute(prepare_query('''
                 UPDATE gifts
