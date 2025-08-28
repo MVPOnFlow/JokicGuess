610ec1206f0523e12900aeb9c7949fb9b6bc6158
bogdang989 <bgavra989@gmail.com>
2025-08-28 23:20:23 +0200

    Vote update

--- react-build/assets/index-B7r8VZMP.js
+++ react-build/assets/index-B7r8VZMP.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-DM5H6ipI.js";import{k as fo}from"./index-wokGeiCV.js";/**
+import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-CH-N0qvj.js";import{k as fo}from"./index-DRwi8xwY.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
